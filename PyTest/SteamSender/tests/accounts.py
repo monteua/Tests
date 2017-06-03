03264b37fed7cf5df2efce61cc34c6675302d5cf
Vadym Stavskyi <arximed.monte@gmail.com>
2017-06-03 12:33:51 +0300

    Update accounts.py

--- PyTest/SteamSender/tests/accounts.py
+++ PyTest/SteamSender/tests/accounts.py
@@ -1,3 +1,3 @@
 def accounts():
-    # list of accounts
-    return ['redacted', 'redacted', 'redacted']
+    return ['monte_ua', 'monte_ru', 'monte_ua2', 'monte_ua3', 'monte_ua4', 'monte_ua5', 'monte_ua6', 'monte_ua7',
+            'monte_ua8', 'monte_ua9', 'monte_ua10', 'monte_ua13']
