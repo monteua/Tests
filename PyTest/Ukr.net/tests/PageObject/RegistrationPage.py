8a8e066f946779e65d02d753b6aa6b28068e3fb9
VadymStavskyi <arximed.monte@gmail.com>
2017-06-05 13:50:02 +0300

    New tests

--- PyTest/Ukr.net/tests/PageObject/RegistrationPage.py
+++ PyTest/Ukr.net/tests/PageObject/RegistrationPage.py
@@ -1,16 +1,17 @@
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-import time
+from selenium.webdriver.common.keys import Keys
 
 # locators
 url = 'https://oauth.ukr.net/registration?lang=en'
 
 # By.XPATH
 mailboxName_loc = '//*[contains(@class, "field-login")]'
-name_taken = '//p[1]'
-
-
-
+password_loc = '//*[contains(@class, "field-password")]'
+password_confirm_loc = '//*[contains(@class, "field-password2")]'
+# errors locators
+name_taken = '//p[@data-lang="login_dropdown_login_error_title"]'
+invalid_password_loc = '//div[@data-lang="password_complexity_error"]'
+mismatch_password_loc = '//div[@data-lang="password_error"]'
 
 class RegistrationPage(object):
 
@@ -22,12 +23,28 @@ class RegistrationPage(object):
 
     def fill_mailbox_name(self, username):
         # element have a dynamic id and name
-        self.driver.find_element(By.XPATH, mailboxName_loc).send_keys(username)
-
-    def mailbox_name_not_taken(self):
+        self.driver.find_element(By.XPATH, mailboxName_loc).send_keys(username, Keys.TAB)
         try:
             error = self.driver.find_element(By.XPATH, name_taken).text
             if 'Unfortunately, someone already got that mailbox' in error:
                 return False
-        except NoSuchElementException:
+        finally:
+            return True
+
+    def fill_password_field(self, password):
+        self.driver.find_element(By.XPATH, password_loc).send_keys(password, Keys.TAB)
+        try:
+            error = self.driver.find_element(By.XPATH, invalid_password_loc).text
+            if 'Your password is insecure.' in error:
+                return False
+        finally:
+            return True
+
+    def fill_confirm_password_field(self, password):
+        self.driver.find_element(By.XPATH, password_confirm_loc).send_keys(password, Keys.TAB)
+        try:
+            error = self.driver.find_element(By.XPATH, mismatch_password_loc).text
+            if 'These passwords don’t match.' in error:
+                return False
+        finally:
             return True
--- PyTest/Ukr.net/tests/test_RegistrationPage.py
+++ PyTest/Ukr.net/tests/test_RegistrationPage.py
@@ -1,10 +1,37 @@
 from PageObject.RegistrationPage import RegistrationPage as RP
+from random import randint
 
-username_valid = 'testdata466'
+username_valid = 'mega_test_data' + str(randint(1000, 100000))
+username_valid2 = 'mega_test_data' + str(randint(1000, 100000))
+username_valid3 = 'mega_test_data' + str(randint(1000, 100000))
+password_valid = 'xQf33Xmp'
 
 
 # enter the valid email, no issue should occur
 def test_enter_valid_username(driver):
     RP(driver).open_page()
     result = RP(driver).fill_mailbox_name(username_valid)
-    assert not result
+    assert result
+
+# in order to have access to the password field, first we must enter valid username
+# valid username + valid password
+def test_enter_val_pass(driver):
+    RP(driver).open_page()
+    RP(driver).fill_mailbox_name(username_valid2)
+
+    password_result = RP(driver).fill_password_field(password_valid)
+    assert password_result
+
+# valid username + valid password + valid password confirmation
+def test_enter_val_pass_val_conf(driver):
+    RP(driver).open_page()
+    RP(driver).fill_mailbox_name(username_valid3)
+    RP(driver).fill_password_field(password_valid)
+
+    password_result = RP(driver).fill_confirm_password_field(password_valid)
+    assert password_result
+
+
+
+
+
