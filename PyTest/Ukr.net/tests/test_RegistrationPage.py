03cf8070203d1cc2d9f934de59e28b967a97b384
VadymStavskyi <arximed.monte@gmail.com>
2017-06-08 12:13:40 +0300

    Fixed some issues + new tests

--- PyTest/Ukr.net/tests/test_RegistrationPage.py
+++ PyTest/Ukr.net/tests/test_RegistrationPage.py
@@ -4,6 +4,7 @@ from random import randint
 username_valid = 'mega_test_data' + str(randint(1000, 100000))
 username_valid2 = 'mega_test_data' + str(randint(1000, 100000))
 username_valid3 = 'mega_test_data' + str(randint(1000, 100000))
+username_valid4 = 'mega_test_data' + str(randint(1000, 100000))
 password_valid = 'xQf33Xmp'
 
 
@@ -12,6 +13,14 @@ def test_enter_valid_username(driver):
     RP(driver).open_page()
     result = RP(driver).fill_mailbox_name(username_valid)
     assert result
+
+
+# enter the already taken username
+def test_enter_taken_username(driver):
+    RP(driver).open_page()
+    result = RP(driver).fill_mailbox_name("test")
+    assert not result
+
 
 # in order to have access to the password field, first we must enter valid username
 # valid username + valid password
@@ -22,6 +31,7 @@ def test_enter_val_pass(driver):
     password_result = RP(driver).fill_password_field(password_valid)
     assert password_result
 
+
 # valid username + valid password + valid password confirmation
 def test_enter_val_pass_val_conf(driver):
     RP(driver).open_page()
@@ -30,6 +40,14 @@ def test_enter_val_pass_val_conf(driver):
 
     password_result = RP(driver).fill_confirm_password_field(password_valid)
     assert password_result
+
+
+# valid username + invalid password
+def test_enter_invalid_password(driver):
+    RP(driver).open_page()
+    RP(driver).fill_mailbox_name(username_valid4)
+    field1 = RP(driver).fill_password_field("x")
+    assert not field1
 
 
 
