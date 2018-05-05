a96debdca6681316f48c54d241e240c062c0bc59
VadymStavskyi <arximed.monte@gmail.com>
2018-05-05 20:37:17 +0300

    New tests

--- PyTest/ecommerce/tests/pages/HomePage.py
+++ PyTest/ecommerce/tests/pages/HomePage.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
-import pytest
+
 # url
 base_url = "http://automationpractice.com/"
 
 # locators
 header_banner = "//*[@class='img-responsive']"
 phone_number = "shop-phone"
-
+contact_us_loc = "contact-link"
+sign_in = "header_user_info"
 
 
 class HomePage(object):
@@ -28,6 +29,18 @@ class HomePage(object):
     def is_header_phone_number_visible(self, param):
         number = self.driver.find_element_by_class_name(phone_number).text
         if number:
-            if param in number:
+            if param in number.lower():
                 return True
             return False
+
+    @property
+    def is_contact_us_visible_and_clickable(self):
+        button = self.driver.find_element_by_id(contact_us_loc).text
+        self.driver.find_element_by_id(contact_us_loc).click()
+        return dict(button=button, title=self.driver.title, url=self.driver.current_url)
+
+    @property
+    def is_sign_in_visible_and_clickable(self):
+        button = self.driver.find_element_by_class_name(sign_in).text
+        self.driver.find_element_by_class_name(sign_in).click()
+        return dict(button_text=button, title=self.driver.title, url=self.driver.current_url)
--- PyTest/ecommerce/tests/test_HomePage.py
+++ PyTest/ecommerce/tests/test_HomePage.py
@@ -1,19 +1,19 @@
 from pages.HomePage import HomePage as Page
 import pytest
 # variables
-page_title = "My Store"
+page_title = "my store"
 opened_url = "http://automationpractice.com/index.php"
-phone_number_text = "Call us now: 0123-456-789"
+phone_number_text = "call us now: 0123-456-789"
 
 
 # Test 1: verify if page fully loads with correct title and desired url opened
 def test_page_loaded(driver):
     data = Page(driver).open_url
     print("Page title: " + data["title"])
-    assert data["title"] == page_title
+    assert data["title"].lower() == page_title
 
     print("Current url: " + data["url"])
-    assert data["url"] == opened_url
+    assert data["url"].lower() == opened_url
 
 
 # Test 2: verify that sale banner in header is displayed
@@ -32,4 +32,25 @@ def test_phone_number_in_header(driver, param):
     assert phone_number
     print("Phone number displayed correctly")
 
+
+# Test 4: verify availability of Contact us button and if correct page opens after click
+def test_contact_us_button(driver):
+    Page(driver).open_url
+    contact_us = Page(driver).is_contact_us_visible_and_clickable
+
+    assert "contact us" in contact_us["button"].lower()
+    print("Contact us button located")
+    assert "contact us" in contact_us["title"].lower()
+    print("Contact page opened, current url: " + contact_us["url"])
+
+
+# Test 5: verify availability of Sign In button and page
+def test_sign_in_button(driver):
+    Page(driver).open_url
+    sign_in = Page(driver).is_sign_in_visible_and_clickable
+
+    assert "sign in" in sign_in["button_text"].lower()
+    print("Sign In button located")
+    assert "login" in sign_in["title"].lower()
+    print("Login page opened, current url: " + sign_in["url"])
 
